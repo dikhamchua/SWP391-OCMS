ce8845f20dc06fa062ada8d55c75904712667e2d
foruser <vinhpham2761.gmail.com>
2025-03-20 12:16:25 +0700

    Enhance ManageCourseController to include section and lesson retrieval
    - Added SectionDAO and LessonDAO to ManageCourseController for managing sections and lessons associated with a course.
    - Implemented logic to retrieve sections by course ID and lessons by section ID, storing lessons in a hashmap categorized by section ID.
    - Updated request attributes to include sections and lessons for improved course content management.

--- src/main/java/com/ocms/controller/dashboard/admin/ManageCourseController.java
+++ src/main/java/com/ocms/controller/dashboard/admin/ManageCourseController.java
@@ -8,16 +8,23 @@ import jakarta.servlet.ServletException;
 import java.io.IOException;
 import com.ocms.dal.*;
 import com.ocms.entity.*;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 @WebServlet("/manage-course")
 public class ManageCourseController extends HttpServlet {
     
 
     private CourseDAO courseDAO;
+    private SectionDAO sectionDAO;
+    private LessonDAO lessonDAO;
 
     @Override
     public void init() throws ServletException {
         courseDAO = new CourseDAO();
+        sectionDAO = new SectionDAO();
+        lessonDAO = new LessonDAO();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -41,8 +48,20 @@ public class ManageCourseController extends HttpServlet {
 
         //get course by id
         Course course = courseDAO.findById(courseId);
+        //get sections by course id
+        List<Section> sections = sectionDAO.getByCourseId(courseId);
+        //get lessons by section id
+        List<Lesson> lessons = lessonDAO.getBySectionId(sections.get(0).getId());
+        
+        //hashmap to store lessons by section id
+        Map<Integer, List<Lesson>> lessonsBySectionId = new HashMap<>();
+        for (Section section : sections) {
+            lessonsBySectionId.put(section.getId(), lessonDAO.getBySectionId(section.getId()));
+        }
 
         request.setAttribute("course", course);
+        request.setAttribute("sections", sections);
+        request.setAttribute("lessonsBySectionId", lessonsBySectionId);
         request.getRequestDispatcher("/view/dashboard/admin/course-content.jsp").forward(request, response);
     }
     
